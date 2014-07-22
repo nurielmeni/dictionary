3ce81d3821b2a34e2d0df0cc2edf22345d98377f
02865824 <02865824@N73495.bezeq.com>
2014-07-22 12:03:34 +0300

    nurielmeni

--- protected/controllers/UserController.php
+++ protected/controllers/UserController.php
@@ -46,7 +46,7 @@ class UserController extends Controller
 			),
 			array('allow', // allow admin user to perform 'admin' and 'delete' actions
 				'actions'=>array('admin','delete'),
-				'users'=>array('meni'),
+				'users'=>array('nurielmeni'),
 			),
 			array('deny',  // deny all users
 				'users'=>array('*'),
